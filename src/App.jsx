ec11b99ca85814dcfed97ac4dd4a5b0ba24539d2
Animesh-Chakrabarty <animesh_chakrabarty@outlook.com>
2023-09-09 15:29:36 +0530

    added hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import "./App.css"
-import About from "./components/About"
-import Hero from "./components/Hero"
-
+import "./App.css";
+import Summarizer  from "./components/Summarizer";
+import Hero from "./components/Hero";
 
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <main>
+      <div className="main">
+        <div className="gradient" />
+      </div>
+      <div className="app">
+        <Hero/>
+        <Summarizer/>
+      </div>
+    </main>
+  );
+};
 
-export default App
+export default App;
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { logo } from "../assets";
 
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <header className="w-full flex flex-col items-center justify-center">
+      <nav className="w-full flex justify-between items-center mt-5 mb-10">
+        <img src={logo} alt="logo" className="w-28 object-contain" />
+        <button
+          type="button"
+          onClick={() =>
+            window.open(
+              "https://github.com/Animesh-Chakrabarty/project_article_summarizer"
+            )
+          }
+          className="black_btn"
+        >
+          Github
+        </button>
+      </nav>
+      <h1 className="head_text">
+        Summarize Articles with
+        <br />
+        <span className="orange_gradient">OpenAI GPT-4</span>
+      </h1>
+      <h2 className="desc">
+        Simplify your reading with Summize, an open-source article summarizer
+        that transforms lengthy articles into clear and concise summaries
+      </h2>
+    </header>
+  );
+};
 
-export default Hero
+export default Hero;
