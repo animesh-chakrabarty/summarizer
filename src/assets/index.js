044558dcf3e9b46ec7e5a4d86acef1bcf7832cab
Animesh-Chakrabarty <animesh_chakrabarty@outlook.com>
2023-09-10 00:10:59 +0530

    completed

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,15 +1,7 @@
-import linkIcon from './link.svg'
-import loader from './loader.svg'
-import copy from './copy.svg'
-import logo from './logo.svg'
-import tick from './tick.svg'
-import logo_2 from './logo_2.png'
+import linkIcon from "./link.svg";
+import loader from "./loader.svg";
+import copy from "./copy.svg";
+import logo from "./logo.svg";
+import tick from "./tick.svg";
 
-export {
-    linkIcon,
-    loader,
-    copy,
-    logo,
-    tick,
-    logo_2
-}
+export { linkIcon, loader, copy, logo, tick };
